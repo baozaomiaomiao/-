c57e24f97ca805fb28c4e70480cb7aa86e8b8a5c
暴躁喵喵 <42052002+baozaomiaomiao@users.noreply.github.com>
2019-08-14 13:01:52 +0800

    Add files via upload

--- 1.栈/index.ts
+++ 1.栈/index.ts
@@ -1,3 +1,66 @@
+<<<<<<< HEAD
+interface IStack<T> {
+   push(element: T): void;
+   pop(): T;
+   peek(): T;
+   isEmpty(): boolean;
+   clear(): void;
+   size(): number;
+   print(): void;
+}
+
+// 创建一个为 栈类。约束栈里面元素的类型
+class _Stack<T> implements IStack<T>{
+   // 私有属性
+   private stack: T[];
+
+   // 添加元素。将元素压入到栈
+   push(element: T): void {
+      this.stack.push(element);
+   }
+
+   // 移除栈顶元素，并返回
+   pop(): T {
+      if (this.isEmpty()) {
+         throw new SyntaxError('Stack is empty, please add elements using push()');
+      }
+      return this.stack.pop();
+   }
+
+   // 查看栈顶元素
+   peek(): T {
+      if (this.isEmpty()) {
+         throw new SyntaxError('The stack is empty');
+      }
+      return this.stack[this.stack.length - 1];
+   }
+
+   // 查看栈是否为空
+   isEmpty(): boolean {
+      return this.stack.length === 0;
+   }
+
+   // 清空栈
+   clear(): void {
+      this.stack = [];
+   }
+
+   // 获取栈的大小
+   size(): number {
+      return this.stack.length;
+   }
+
+   // 打印栈数据
+   print(): void {
+      if (this.isEmpty()) {
+         throw new SyntaxError('The stack is empty');
+      }
+      console.log(this.stack.toString());
+   }
+}
+
+
+=======
 // 创建一个为 栈类
 class _Stack {
    // 私有属性
@@ -52,3 +115,4 @@ class _Stack {
 }
 
 
+>>>>>>> afb56d3b14fe2e7702d7a4a3ead718f78ca53552
